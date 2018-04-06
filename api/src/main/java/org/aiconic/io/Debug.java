9a80b0a1863beb729274b9d9e7bd0567a0b916a5
Jasbir Shah <JasbirShah@outlook.com>
2018-04-06 14:13:29 +1000

    Changed Debug to check against any system properties
    To enable debugging set -Ddebug="true" from the JVM.

--- api/src/main/java/org/aiconic/io/Debug.java
+++ api/src/main/java/org/aiconic/io/Debug.java
@@ -1,10 +1,23 @@
 package org.aiconic.io;
 
-public class Debug
-{
-    public static void out(String out)
-    {
-        if (true) {
+/**
+ * A utility class for printing debug statements.
+ */
+public class Debug {
+
+    /**
+     * Prints the provided output to the standard error outputstream
+     * if the debug flag is set.
+     * <p>
+     * Example configuration:
+     * <pre>{@code
+     * java -Ddebug="true" my.jar %1
+     * }
+     * </pre>
+     * @param      output   The output to print
+     */
+    public static void out(String output) {
+        if (Boolean.getBoolean("debug")) {
             System.err.println(out);
         }
     }
