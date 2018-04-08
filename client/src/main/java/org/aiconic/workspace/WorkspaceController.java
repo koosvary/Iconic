df9291bc34c1011bd687bddfaf169167a8299e08
Jasbir Shah <JasbirShah@outlook.com>
2018-04-08 21:35:45 +1000

    Removed Controller base class
    It's not that useful anymore. I also actually upgraded the Gradle Wrapper this time...

--- client/src/main/java/org/aiconic/workspace/WorkspaceController.java
+++ client/src/main/java/org/aiconic/workspace/WorkspaceController.java
@@ -1,4 +1,20 @@
 package org.aiconic.workspace;
 
-public class WorkspaceController {
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import org.aiconic.model.DatasetModel;
+import org.aiconic.model.GlobalModel;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class WorkspaceController implements Initializable {
+
+    public WorkspaceController() {
+    }
+
+    @Override
+    public void initialize(URL arg1, ResourceBundle arg2) {
+
+    }
 }
