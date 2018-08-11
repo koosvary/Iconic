322dc0cc12ff8e9220de6be9888051059156cb8f
Jack Newley <ubjinn@gmail.com>
2018-08-11 19:20:37 +1000

    Addition of natural log + its test case. Revision of Root + an addition of a test case

---
+++ api/src/main/java/org/iconic/ea/operator/primitive/NaturalLog.java
@@ -0,0 +1,10 @@
+package org.iconic.ea.operator.primitive;
+
+public class NaturalLog extends ArithmeticPrimitive<Number> {
+    public NaturalLog() {
+        super(
+                args -> Math.log(args.get(0)),
+                1, "+"
+        );
+    }
+}
---
+++ api/src/test/java/org/iconic/ea/operator/primitive/RootTest.java
@@ -0,0 +1,45 @@
+package org.iconic.ea.operator.primitive;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+
+import static java.lang.Double.NaN;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class RootTest {
+    @DisplayName("Test root using doubles")
+    @MethodSource("doubleListProvider")
+    @ParameterizedTest
+    void addDoublesTest(final List<Double> args, final double expected) {
+        final FunctionalPrimitive<Double, Double> add = new Root();
+        final double delta = 0.001d;
+        final double actual = add.apply(args);
+        assertEquals(expected, actual, delta);
+    }
+
+    /**
+     * <p>
+     * Returns a stream of double n-tuples, where the last member of the tuple is the sum of all the preceeding
+     * members
+     * </p>
+     *
+     * @return a stream of double n-tuples
+     */
+    private static Stream<Arguments> doubleListProvider() {
+        return Stream.of(
+                Arguments.of(Arrays.asList(1.d, 1.d), 1.d),
+                Arguments.of(Arrays.asList(27.d, 3.d), 3.d),
+                Arguments.of(Arrays.asList(-2.d, -2.d), 1.d),
+                Arguments.of(Arrays.asList(2.d, -2.d), 1.d),
+                Arguments.of(Arrays.asList(-2.d, 2.d), NaN),
+                Arguments.of(Arrays.asList(2.d, 2.d), 1.4142135623731)
+
+        );
+    }
+}
