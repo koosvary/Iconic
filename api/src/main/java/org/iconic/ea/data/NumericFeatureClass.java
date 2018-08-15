4b5ea7e8310c078093df3330ffe2a453716009ed
Harry <harry2114spam@live.com>
2018-08-15 11:04:08 +1000

    Created the offset preprocessing feature.

--- api/src/main/java/org/iconic/ea/data/NumericFeatureClass.java
+++ api/src/main/java/org/iconic/ea/data/NumericFeatureClass.java
@@ -1,10 +1,12 @@
 package org.iconic.ea.data;
 
 import org.iconic.ea.data.preprocessing.Normalise;
+import org.iconic.ea.data.preprocessing.Offset;
 
 public class NumericFeatureClass extends FeatureClass<Number> {
     public NumericFeatureClass(boolean output) {
         super(output);
         getPreprocessors().add(new Normalise());
+        getPreprocessors().add(new Offset());
     }
 }
