b7d152a0c7f4bbce332cb5305d685da68c796adf
Chris <cro52@hotmail.com>
2018-08-15 17:38:37 +1000

    Adding more support classes for CGP and continuing development of main cgp strategy class

---
+++ api/src/main/java/org/iconic/ea/operator/evolutionary/mutation/cgp/CartesianMutator.java
@@ -0,0 +1,17 @@
+package org.iconic.ea.operator.evolutionary.mutation.cgp;
+
+import org.iconic.ea.chromosome.cartesian.CartesianChromosome;
+import org.iconic.ea.operator.evolutionary.mutation.Mutator;
+import org.iconic.ea.operator.primitive.FunctionalPrimitive;
+
+import java.util.List;
+
+public class CartesianMutator<R> implements Mutator<CartesianChromosome<R>, R>{
+	@Override
+	public CartesianChromosome<R> apply(final List<FunctionalPrimitive<R, R>> functionalPrimitives,
+										final CartesianChromosome<R> chromosome){
+		assert (chromosome.getGenome().size() > 0);
+		final CartesianChromosome mutant = chromosome.clone();
+		return null;
+	}
+}
--- api/src/main/java/org/iconic/ea/strategies/cgp/CartesianGeneticProgramming.java
+++ api/src/main/java/org/iconic/ea/strategies/cgp/CartesianGeneticProgramming.java
@@ -1,9 +1,12 @@
 package org.iconic.ea.strategies.cgp;
 
 import org.iconic.ea.EvolutionaryAlgorithm;
+import org.iconic.ea.chromosome.Chromosome;
 import org.iconic.ea.chromosome.cartesian.CartesianChromosome;
 import org.iconic.ea.chromosome.cartesian.CartesianChromosomeFactory;
+import org.iconic.ea.chromosome.expression.ExpressionChromosome;
 
+import java.util.Comparator;
 import java.util.List;
 
 public class CartesianGeneticProgramming<T> extends EvolutionaryAlgorithm<CartesianChromosome<T>, T>{
@@ -16,12 +19,23 @@ public class CartesianGeneticProgramming<T> extends EvolutionaryAlgorithm<Cartes
 
 	@Override
 	public void initialisePopulation(int populationSize, int numFeatures){
-
+		for (int i = 0; i < populationSize; i++) {
+			Chromosome<T> chromosome = chromosomeFactory.getChromosome();
+			getObjective(0).apply(chromosome);
+			getChromosomes().add((CartesianChromosome<T>) chromosome);
+		}
 	}
 
 	@Override
 	public List<CartesianChromosome<T>> evolve(List<CartesianChromosome<T>> population){
-		return null;
+		final Comparator<Chromosome<T>> comparator = Comparator.comparing(Chromosome::getFitness);
+		final CartesianChromosome<T> bestCandidate = population
+				.stream().min(comparator).get();
+
+		for(int populationIndex = 1; populationIndex < population.size(); populationIndex++){
+			CartesianChromosome<T> c = population.get(populationIndex);
+			population.set(populationIndex, mutate(c));
+		}
 	}
 
 	public CartesianChromosome<T> mutate(CartesianChromosome<T> chromosome){
