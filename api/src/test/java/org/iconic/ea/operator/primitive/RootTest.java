cbbffc89c194ae6ec014ac669bb5746f61ce7c75
Jack Newley <ubjinn@gmail.com>
2018-08-21 18:14:58 +1000

    Adjusted some tests to be more robust.

--- api/src/test/java/org/iconic/ea/operator/primitive/RootTest.java
+++ api/src/test/java/org/iconic/ea/operator/primitive/RootTest.java
@@ -1,6 +1,5 @@
 package org.iconic.ea.operator.primitive;
 
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
