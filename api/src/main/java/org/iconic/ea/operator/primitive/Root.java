3fb3a1539d505929d2f3362c4dd6c9bf047b5c20
Jack Newley <ubjinn@gmail.com>
2018-08-21 15:36:09 +1000

    Root now works as expected according to test cases in RootTest. The test case doesnt have a value for negative numbers with even roots (ie -1^(1/2))

--- api/src/main/java/org/iconic/ea/operator/primitive/Root.java
+++ api/src/main/java/org/iconic/ea/operator/primitive/Root.java
@@ -1,14 +1,23 @@
 package org.iconic.ea.operator.primitive;
 
+import static java.lang.Math.abs;
+
 public class Root extends ArithmeticPrimitive<Number> {
     public Root() {
         super(
                 args -> {
-                    //TODO why delta here?
                     final double delta = 0.001;
-                    return (args.get(1) >= 0 + delta && args.get(1) >= 0 - delta)
-                            ? Math.pow(args.get(0), 1 / args.get(1))
-                            : 1;
+                    if (args.get(0) >= 0 + delta && args.get(0) >= 0 - delta) {
+                        return Math.pow(args.get(0), 1 / args.get(1));
+                    } else if (args.get(0) < 0 - delta) {
+                        double result = Math.pow(abs(args.get(0)), 1 / args.get(1));
+                        if (args.get(1) % 2 == 0) {
+                            return result;
+                        } else {
+                            return -result;
+                        }
+                    }
+                    return Double.POSITIVE_INFINITY;
                 },
                 2, "ROOT"
         );
