9ebafda417e4972427d3859c092ee5c989dc4d81
ubjinn@gmail.com <Qudepimu2@>
2018-08-21 19:22:36 +1000

    Arity on Equal To was wrong.

--- api/src/main/java/org/iconic/ea/operator/primitive/EqualTo.java
+++ api/src/main/java/org/iconic/ea/operator/primitive/EqualTo.java
@@ -4,7 +4,7 @@ public class EqualTo extends ArithmeticPrimitive<Number> {
     public EqualTo() {
         super(
                 args -> args.get(0).equals(args.get(1)) ? 1.d : 0.d,
-                1, "=="
+                2, "=="
         );
     }
 }
