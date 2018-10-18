190301e5d7f3299cbd810e8397e7fd556a1e2998
Harry <harry2114spam@live.com>
2018-10-18 15:54:21 +1100

    Now graphs the expected results in Results tab

--- client/src/main/java/org/iconic/project/results/ResultsController.java
+++ client/src/main/java/org/iconic/project/results/ResultsController.java
@@ -19,7 +19,7 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
  * SOFTWARE.
  */
-package org.iconic.project.results;
+        package org.iconic.project.results;
 
 import com.google.inject.Inject;
 import javafx.application.Platform;
@@ -27,6 +27,8 @@ import javafx.beans.InvalidationListener;
 import javafx.collections.FXCollections;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.chart.LineChart;
+import javafx.scene.chart.XYChart;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.TextFieldTableCell;
@@ -35,19 +37,15 @@ import javafx.util.converter.IntegerStringConverter;
 import lombok.extern.log4j.Log4j2;
 import org.iconic.control.WorkspaceTab;
 import org.iconic.ea.chromosome.Chromosome;
-import org.iconic.ea.chromosome.expression.ExpressionChromosome;
-import org.iconic.ea.operator.primitive.FunctionalPrimitive;
+import org.iconic.ea.data.DataManager;
+import org.iconic.ea.data.FeatureClass;
 import org.iconic.project.Displayable;
-import org.iconic.project.dataset.DatasetModel;
-import org.iconic.project.search.io.SearchExecutor;
-import org.iconic.project.search.SearchService;
 import org.iconic.project.search.SolutionStorage;
 import org.iconic.project.search.config.SearchConfigurationModel;
 import org.iconic.workspace.WorkspaceService;
 
 import java.net.URL;
 import java.util.*;
-import java.util.stream.Collectors;
 
 /**
  * A controller for the Results view
@@ -65,6 +63,9 @@ public class ResultsController implements Initializable {
     private WorkspaceTab resultsTab;
     @FXML
     private TableView<ResultDisplay> solutionsTableView;
+    @FXML
+    private LineChart<Number,Number> solutionsPlot;
+
 
     /**
      * Constructs a new ResultsController that attaches an invalidation listener onto the workspace service.
@@ -86,6 +87,13 @@ public class ResultsController implements Initializable {
         updateWorkspace();
 
         resultsTab.setOnSelectionChanged(event -> updateWorkspace());
+
+        // Listener for the solutions being clicked in the table
+        solutionsTableView.getSelectionModel().selectedItemProperty().addListener((obs, oldSelection, newSelection) -> {
+            if (newSelection != null) {
+                graphExpectedValues();
+            }
+        });
     }
 
     /**
@@ -117,6 +125,68 @@ public class ResultsController implements Initializable {
         }
 
         Platform.runLater(this::updateWorkspaceMainThread);
+    }
+
+    public void graphExpectedValues() {
+        System.out.println("ResultsController   graphExpectedValues");
+        Displayable item = getWorkspaceService().getActiveWorkspaceItem();
+
+        // If no dataset, stop what you're doing.
+        if (!(item instanceof SearchConfigurationModel)) {
+            // TODO clear the UI?
+            return;
+        }
+
+        SearchConfigurationModel search = (SearchConfigurationModel) item;
+
+        // Dataset model is not present
+        if (!search.getDatasetModel().isPresent()) {
+            return;
+        }
+
+        // Get the dataset
+        DataManager dataManager = search.getDatasetModel().get().getDataManager();
+        HashMap<String, FeatureClass<Number>> dataset = dataManager.getDataset();
+
+        // Get the first output Feature we find
+        FeatureClass<Number> outputFeatureClass = null;
+        for (FeatureClass<Number> featureClass : dataset.values()) {
+            if (featureClass.isOutput()) {
+                outputFeatureClass = featureClass;
+                break;
+            }
+        }
+
+        // No output feature class found.
+        if (outputFeatureClass == null) {
+            log.info("No output feature class was found in the dataset to be used to graph expected results vs actual results");
+            return;
+        }
+
+
+        // Create the two new lines
+        XYChart.Series series1 = new XYChart.Series();
+        XYChart.Series series2 = new XYChart.Series();
+
+        // Set the labels for the legend
+        series1.setName("Expected Values");
+        series2.setName("Actual Values");
+
+        // Get all the expected values
+        List<Number> samples = outputFeatureClass.getSamples();
+        for (int i = 0; i < samples.size(); i++) {
+            series1.getData().add(new XYChart.Data(i, samples.get(i)));
+        }
+
+        // TODO - Get the actual values
+        //series2.getData().add(new XYChart.Data(i, samples.get(i)));
+
+
+        solutionsPlot.setAnimated(false);
+        solutionsPlot.setCreateSymbols(true);
+
+        solutionsPlot.getData().clear();
+        solutionsPlot.getData().addAll(series1, series2);
     }
 
     /**
@@ -158,5 +228,26 @@ public class ResultsController implements Initializable {
      */
     private WorkspaceService getWorkspaceService() {
         return workspaceService;
+    }
+
+    public void solutionPlotTest() {
+        System.out.println("ResultsController   solutionPlotTest");
+
+        XYChart.Series series1 = new XYChart.Series();
+        series1.setName("Equities");
+        series1.getData().add(new XYChart.Data(4.2, 193.2));
+        series1.getData().add(new XYChart.Data(2.8, 33.6));
+        series1.getData().add(new XYChart.Data(6.8, 23.6));
+
+        XYChart.Series series2 = new XYChart.Series();
+        series2.setName("Mutual funds");
+        series2.getData().add(new XYChart.Data(5.2, 229.2));
+        series2.getData().add(new XYChart.Data(2.4, 37.6));
+        series2.getData().add(new XYChart.Data(6.4, 15.6));
+
+        solutionsPlot.setAnimated(false);
+        solutionsPlot.setCreateSymbols(true);
+
+        solutionsPlot.getData().addAll(series1, series2);
     }
 }
